9decb6156407fb9d7713bb2f34c2b254f4ba84a6
Mikhailo Kholmohorov <mykhailo1257@gmail.com>
2020-08-24 20:05:20 +0300

    lab1 create solution

---
+++ Lab1Track/TextFormat.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1Track
+{
+    class Paragraph
+    {
+        public string TypeOfParagraph { get; set; }
+    }
+    class Header
+    {
+        public string HeaderFirstLevel { get; set; }
+        public string HeaderSecondLevel { get; set; }
+        public string HeaderThirdLevel { get; set; }
+        public string HeaderFourthLevel { get; set; }
+        public string HeaderFifthLevel { get; set; }
+        public string HeaderSixthLevel { get; set; }
+
+    }
+
+    class OrderList
+    {
+        public string TypeOfOrderList { get; set; }
+    }
+
+    class BulletedList
+    {
+        public string TypeOfBulletedList { get; set; }
+    }
+    class TextFormat
+    {
+        public UserText Text { get; set; }
+        public Paragraph Paragraph { get; set; }
+        public Header Header { get; set; }
+        public OrderList OrderList { get; set; }
+        public BulletedList BulletedList { get; set; }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (Paragraph != null)
+                sb.Append(Paragraph.TypeOfParagraph +);
+            if (Salt != null)
+                sb.Append("Соль \n");
+            if (Additives != null)
+                sb.Append("Добавки: " + Additives.Name + " \n");
+            return sb.ToString();
+        }
+    }
+}
---
+++ Lab1Track/UserText.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1Track
+{
+    class ParagraphText
+    {
+        public string TextOfParagraph { get; set; }
+    }
+    class HeaderText
+    {
+        public string TextOfHeader1 { get; set; }
+        public string TextOfHeader2 { get; set; }
+        public string TextOfHeader3 { get; set; }
+        public string TextOfHeader4 { get; set; }
+        public string TextOfHeader5 { get; set; }
+        public string TextOfHeader6 { get; set; }
+
+    }
+
+    class OrderListText
+    {
+        public string TextOfOrderList { get; set; }
+    }
+
+    class BulletedListText
+    {
+        public string TextOfBulletedList { get; set; }
+    }
+    class UserText
+    {
+        public ParagraphText ParagraphText { get; set; }
+        public HeaderText HeaderText { get; set; }
+        public OrderListText OrderListText { get; set; }
+        public BulletedListText BulletedListText { get; set; }
+        public UserText (string InputUserText)
+        {
+            if(InputUserText.Contains("<par>"))
+            {
+                this.ParagraphText.TextOfParagraph = InputUserText.Replace("<par>", "");
+            }
+            else if (InputUserText.Contains("<Olist>"))
+            {
+                this.OrderListText.TextOfOrderList = InputUserText.Replace("<Olist>","");
+                while (Console.ReadLine() != null || Console.ReadLine() != "")
+                {
+                    this.OrderListText.TextOfOrderList += Console.ReadLine();
+                }
+            }
+            else if(InputUserText.Contains("<Blist>"))
+            {
+                this.BulletedListText.TextOfBulletedList = InputUserText.Replace("<Blist>", "");
+                while(Console.ReadLine() != null || Console.ReadLine() != "")
+                {
+                    this.BulletedListText.TextOfBulletedList += Console.ReadLine();
+                }
+            }
+        }
+    }
+}
