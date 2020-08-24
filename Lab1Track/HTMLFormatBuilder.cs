f0a49e97f485338f0fb148dbf2909e7a7bc224e2
Mikhailo Kholmohorov <mykhailo1257@gmail.com>
2020-08-24 22:41:43 +0300

    update

--- Lab1Track/HTMLFormatBuilder.cs
+++ Lab1Track/HTMLFormatBuilder.cs
@@ -4,7 +4,35 @@ using System.Text;
 
 namespace Lab1Track
 {
-    class HTMLFormatBuilder
+    class HTMLFormatBuilder : FormatBuilder
     {
+        public override void SetUserText()
+        {
+            this.TextFormat.Text = new UserText(Console.ReadLine());
+        }
+        public override void SetParagraph()
+        {
+            this.TextFormat.Paragraph = new Paragraph { TypeOfParagraph = "<p>" };
+        }
+        public override void SetHeader()
+        {
+            this.TextFormat.Header = new Header
+            {
+                HeaderFirstLevel = "<h1>",
+                HeaderSecondLevel = "<h2>",
+                HeaderThirdLevel = "<h3>",
+                HeaderFourthLevel = "<h4>",
+                HeaderFifthLevel = "<h5>",
+                HeaderSixthLevel = "<h6>"
+            };
+        }
+        public override void SetOrderList()
+        {
+            this.TextFormat.OrderList = new OrderList { TypeOfOrderList = "<ol>" };
+        }
+        public override void SetBulletedList()
+        {
+            this.TextFormat.BulletedList = new BulletedList { TypeOfBulletedList = "<ul>" };
+        }
     }
 }
